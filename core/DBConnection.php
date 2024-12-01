710fc997f9c8921e45722a86f436f00133f9d31f
Cameron Everett <camtrillz@gmail.com>
2024-12-01 14:30:34 -0600

    added env file passwords

---
+++ core/DBConnection.php
@@ -0,0 +1,14 @@
+<?php
+namespace Core;
+use PDO;
+class DBConnection{
+    public static function start(){
+        try{
+         return   $pdo = new PDO("{$_ENV['DB_CONNECTION']}:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_DATABASE']}", $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+          
+          } catch(PDOException $e ) {
+            dd($e-> getMessage());
+          }
+          
+    }
+}
